cfc9201bbc42fbbaab0396e7e5100bbef2e60bbb
Gordon <gordon.boeer@fh-kiel.de>
2021-12-01 13:21:16 +0100

    Day 2021 solution

---
+++ 2021/day1/solve.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+from utils import input
+
+
+def solve(k_size=3):
+    """Solve first or second part of the puzzle"""
+    r_in = input()
+    sw_sum = [sum(r_in[i:i+k_size]) for i in range(len(r_in)-(k_size-1))]
+    return len([True for i in range(1, len(sw_sum)) if sw_sum[i]-sw_sum[i-1] > 0])
+
+
+if __name__ == "__main__":
+
+    print(f"Solution to first puzzle: {solve(k_size=1)}")
+    print(f"Solution to second  puzzle: {solve()}")
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 
+def input(file='input.txt'):
+    return [int(x) for x in Path(file).read_text().splitlines()]
 
 def prod(numbers):
     y = 1
