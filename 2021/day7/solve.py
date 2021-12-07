cf4ea10250320edd6a2e0ad9a7af5556668cab62
Gordon <gordon.boeer@fh-kiel.de>
2021-12-07 10:42:29 +0100

    solve day7

---
+++ 2021/day7/solve.py
@@ -0,0 +1,26 @@
+from utils import input
+import numpy as np
+
+
+def solve1():
+    """Solve first puzzle"""
+    r_in = np.array([int(n) for n in input(convert_fn=lambda x: x.split(','))[0]])
+    fuel = []
+    for i in range(np.max(r_in)+1):
+        fuel.append(np.sum(np.abs(r_in - i)))
+    return np.min(fuel)
+
+
+def solve2():
+    """Solve first puzzle"""
+    r_in = np.array([int(n) for n in input(convert_fn=lambda x: x.split(','))[0]])
+    fuel = []
+    costs = list(range(1, np.max(r_in)+1))
+    for i in range(np.max(r_in)+1):
+        fuel.append(sum([sum(costs[:c]) for c in np.abs(r_in - i)]))
+    return np.min(fuel)
+
+
+if __name__ == "__main__":
+    print(f"Solution to first puzzle: {solve1()}")
+    print(f"Solution to second puzzle: {solve2()}")
