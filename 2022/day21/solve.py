20bfd611d23c43a0fbcc994cdcf892e20a16c273
Gordon <gordon.boeer@fh-kiel.de>
2022-12-21 11:52:53 +0100

    2022/day 21

---
+++ 2022/day21/solve.py
@@ -0,0 +1,62 @@
+from utils import input
+from operator import add, sub, mul, truediv
+
+ops = {'+': add, '-': sub, '*': mul, '/': truediv}
+
+
+def solve_monkeys(monkeys):
+
+    while True:
+        solved = {m: v for m, v in monkeys.items() if type(v) == int}
+        remaining = {m: v for m, v in monkeys.items() if m not in solved}
+        for m, v in remaining.items():
+            m1, op, m2 = v
+            if m1 in solved and m2 in solved:
+                monkeys[m] = int(ops[op](monkeys[m1], monkeys[m2]))
+        if len(remaining) == 0:
+            break
+
+    return monkeys
+
+
+def solve1():
+
+    monkeys = input(convert_fn=lambda x: x.split(': '))
+    monkeys = {m[0]: int(m[1]) if m[1].isnumeric() else m[1].split() for m in monkeys}
+    monkeys = solve_monkeys(monkeys)
+    return monkeys['root']
+
+
+def solve2():
+
+    monkeys = input(convert_fn=lambda x: x.split(': '))
+    monkeys = {m[0]: int(m[1]) if m[1].isnumeric() else m[1].split() for m in monkeys}
+    root = monkeys.pop('root')
+
+    monkeys['humn'] = 1
+    x_d = 1
+    y0 = None
+    while True:
+        monkeys['humn'] += x_d
+        monkeys_cp = solve_monkeys(monkeys.copy())
+        y, v = int(monkeys_cp[root[0]]), int(monkeys_cp[root[2]])
+
+        # initial run
+        if y0 is None:
+            y0 = y
+        else:
+            y_d = y-y0
+            if y_d != 0:
+                r_d = v-y
+                x_d = max(int(r_d / (y_d / x_d)), 1)
+
+        if int(monkeys_cp[root[0]]) == int(monkeys_cp[root[2]]):
+            break
+
+    return monkeys['humn']
+
+
+if __name__ == "__main__":
+
+    print(f"Solution to first puzzle: {solve1()}")
+    print(f"Solution to second puzzle: {solve2()}")
--- utils.py
+++ utils.py
@@ -9,10 +9,20 @@ class Grid:
         self._data = None
 
     def __getitem__(self, item):
-        return self._data[item[0]][item[1]]
+        y = slice(item[0], item[0] + 1, 1) if isinstance(item[0], int) else item[0]
+        x = slice(item[1], item[1] + 1, 1) if isinstance(item[1], int) else item[1]
+        vals = []
+        for r in range(y.start, y.stop):
+            for c in range(x.start, x.stop):
+                vals.append(self._data[r][c])
+        return vals[0] if len(vals) == 1 else vals
 
     def __setitem__(self, key, value):
-        self._data[key[0]][key[1]] = value
+        y = slice(key[0], key[0]+1, 1) if isinstance(key[0], int) else key[0]
+        x = slice(key[1], key[1]+1, 1) if isinstance(key[1], int) else key[1]
+        for r in range(y.start, y.stop):
+            for c in range(x.start, x.stop):
+                self._data[r][c] = value
 
     @property
     def width(self):
@@ -31,10 +41,15 @@ class Grid:
 
     def pad_border(self, value, size):
         """Add border padding of size with the given value."""
-        for i in range(size):
+        if isinstance(size, int):
+            size = [size] * 4
+        for i in range(size[0]):
             self._data.insert(0, [value] * self.width)
+        for i in range(size[1]):
             self._data.insert(self.height, [value] * self.width)
-        self._data = [[[value] * size + r + [value] * size] for r in self._data]
+
+        self._data = [[value] * size[2] + r for r in self._data]
+        self._data = [r + [value] * size[2] for r in self._data]
 
     def add(self, val):
         """Add value to all elements."""
@@ -47,6 +62,13 @@ class Grid:
     def find(self, val):
         """Return the indices of the given value."""
         return [(r, c) for r in range(self.height) for c in range(self.width) if self[(r, c)] == val]
+
+    def print(self, file):
+
+        for r in self._data:
+            file.write("".join([str(s) for s in r])+'\n')
+        file.write('\n')
+
 
     @classmethod
     def from_file(cls, file='input.txt', conv_fn=None):
@@ -99,10 +121,18 @@ class Graph:
         return list(self._edges[vert_i])
 
     def add_edge(self, u, v, weight=1):
+
         if u not in self._edges:
-            self._edges[u] = {v: weight}
-        else:
-            self._edges[u][v] = weight
+            self._edges[u] = {}
+
+        if isinstance(v, int):
+            v = [v]
+        for k in v:
+            self._edges[u][k] = weight
+
+    def print(self):
+        for e, n in self._edges.items():
+            print(f'{e} -> {n}')
 
     @classmethod
     def from_grid(cls, grid: 'Grid', nb_mask: 'Grid', decision_fn):
@@ -207,3 +237,7 @@ def prod(numbers):
     for x in numbers:
         y *= x
     return y
+
+
+def sign(x):
+    return -1 if x < 0 else 1
