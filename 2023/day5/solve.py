1d8daa3e3d71b64fbf86fed993d7c9b2c29c826a
Gordon Böer <Legor@users.noreply.github.com>
2023-12-05 16:37:19 +0100

    Add day 5 part 2 solution

--- 2023/day5/solve.py
+++ 2023/day5/solve.py
@@ -1,117 +1,81 @@
 from utils import input
-from functools import cache, lru_cache
-from tqdm.auto import tqdm
 
 
-def solve():
-    """Solve first part of the puzzle"""
-    lines = input(split_on='\n\n')
-    seeds = map(int, lines[0].split('seeds:')[1].split())
+def get_seeds_and_maps():
+    """parse the input and create the mappings"""
+    blocks = input(split_on='\n\n')
+    seeds = list(map(int, blocks[0].split('seeds:')[1].split()))
     maps = []
-    for m in lines[1:]:
-        map_lines = m.split('\n')
+    for block in blocks[1:]:
+        map_lines = block.split('\n')
         range_map = []
         for range_line in map_lines[1:]:
             dest_start, source_start, length = map(int, range_line.split())
             range_map.append((dest_start, source_start, length))
         maps.append(range_map)
 
+    return seeds, maps
+
+
+def trace_seed(seed, maps):
+    """Follow a seed down the tree"""
+
+    next_seed = seed
+    for m in maps:
+        for range_i in m:
+            dest_start, source_start, length = range_i
+            if source_start <= next_seed < source_start + length:
+                next_seed = dest_start + (next_seed - source_start)
+                break
+    return next_seed
+
+
+def reverse_maps(maps):
+    """Reverse the tree (top->bottom <--> bottom->top)"""
+    return [[(source, dest, length) for dest, source, length in range_map] for range_map in maps[::-1]]
+
+
+def check_seed(seed, seed_ranges):
+    """Check if a seed is in the given seed ranges"""
+    for seed_range in seed_ranges:
+        if seed_range[0] <= seed <= seed_range[0] + seed_range[1]:
+            return True
+    return False
+
+
+def solve():
+    """Solve first part of the puzzle"""
+    seeds, maps = get_seeds_and_maps()
     locations = []
     for seed in seeds:
-        next_seed = seed
-        for m in maps:
-            for range_i in m:
-                dest_start, source_start, length = range_i
-                if source_start <= next_seed < source_start+length:
-                    next_seed = dest_start + (next_seed - source_start)
-                    break
-
-        locations.append(next_seed)
-
+        locations.append(trace_seed(seed, maps))
     return min(locations)
-
-
-seed_map = []
-
-@lru_cache(maxsize=None)
-def get_location(seed):
-    #next_seed = seed
-    #for range_map in seed_map:
-    #    next_seed = get_next_seed(range_map, next_seed)
-    return get_next_seed(0, seed)
-
-
-#@lru_cache(maxsize=10024)
-#def get_next_seed(range_map, seed):
-    #for range_i in range_map:
-    #    dest_start, source_start, length = range_i
-    #    if source_start <= seed < source_start + length:
-    #        return dest_start + (seed - source_start)
-    #return seed
-
-
-
-
-@lru_cache(maxsize=None)
-def get_next_seed(i, seed):
-    next_seed = seed
-    for range_i in seed_map[i]:
-        dest_start, source_start, length = range_i
-        if source_start <= seed < source_start + length:
-
-            next_seed = dest_start + (seed - source_start)
-            break
-
-    if i == len(seed_map)-1:
-        return next_seed
-    #print(i, seed, next_seed)
-    return get_next_seed(i+1, next_seed)
 
 
 def solve2():
-    """Solve first part of the puzzle"""
-    lines = input(split_on='\n\n')
-    seeds = list(map(int, lines[0].split('seeds:')[1].split()))
+    """Solve second part of the puzzle"""
+
+    seeds, maps = get_seeds_and_maps()
     seed_ranges = [(seeds[i], seeds[+1]) for i in range(0, len(seeds), 2)]
-    global seed_map
-    for m in lines[1:]:
-        map_lines = m.split('\n')
-        range_map = []
-        for range_line in map_lines[1:]:
-            dest_start, source_start, length = map(int, range_line.split())
-            range_map.append(tuple([dest_start, source_start, length]))
-        seed_map.append(tuple(range_map))
+    seed_ranges = sorted(seed_ranges, key=lambda r: r[0])
 
-    seed_map = tuple(seed_map)
-    locations = []
-    solved = {}
-    for seed_range in seed_ranges:
-        for seed in tqdm(range(seed_range[0], seed_range[0]+seed_range[1])):
-            #print('Start ', seed)
-            next_seed = get_next_seed(0, seed)
-            info = get_next_seed.cache_info()
-            if info.hits != 0:
-                print(info)
+    # do an inverse search (going from location to seeds)
+    maps = reverse_maps(maps)
 
-            #next_seed = seed
-            #if next_seed not in solved:
-            #    for i_m, m in enumerate(maps):
-            #        for range_i in m:
-            #            dest_start, source_start, length = range_i
-            #            if source_start <= next_seed < source_start+length:
-            #                next_seed = dest_start + (next_seed - source_start)
-            #                break
+    tree = IntervalTree()
+    for start, length in seed_ranges:
+        tree[start:start+length] = True
 
-                #solved[seed] = next_seed
-
-            locations.append(next_seed)
-
-    return min(locations)
-
-
+    max_loc = max([m[0]+m[2] for m in maps[0]])
+    for i in range(max_loc):
+        seed = trace_seed(i, maps)
+        if check_seed(seed, seed_ranges):
+            # found the lowest location which maps to a seed, so we can finish
+            return i
+    return None
 
 
 if __name__ == "__main__":
 
-    print(f"Solution to first puzzle: {solve()}")
-   # print(f"Solution to second  puzzle: {solve2()}")
+   print(f"Solution to first puzzle: {solve()}")
+   print(f"Solution to second  puzzle: {solve2()}")
