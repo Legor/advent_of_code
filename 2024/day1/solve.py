788d28af633066756d5a30cc0024b63efc16c642
Gordon Böer <Legor@users.noreply.github.com>
2024-12-04 10:37:25 +0100

    💄 linting

--- 2024/day1/solve.py
+++ 2024/day1/solve.py
@@ -1,16 +1,23 @@
+# https://adventofcode.com/2024/day/1
+
 from utils import parse_input
+
 
 def solve(left, right):
     """Solve first part of the puzzle"""
-    return sum([abs(le-r) for le, r in zip(left, right)])
+    return sum([abs(le - r) for le, r in zip(left, right)])
+
 
 def solve2(left, right):
     """Solve second part of the puzzle"""
     return sum([right.count(le) * le for le in left])
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     # parse lists
-    left, right = map(lambda l: sorted(l), zip(*parse_input(convert_fn=lambda l: list(map(int, str.split(l))))))
+    left, right = map(
+        lambda line: sorted(line),
+        zip(*parse_input(convert_fn=lambda line: list(map(int, str.split(line))))),
+    )
     print(f"Solution to first puzzle: {solve(left, right)}")
     print(f"Solution to second  puzzle: {solve2(left, right)}")
