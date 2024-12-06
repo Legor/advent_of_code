4e5b46bc832eb12ee4723f63f0f5bb73c090b25b
Gordon Böer <Legor@users.noreply.github.com>
2024-12-06 10:38:29 +0100

    dump changes

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from functools import reduce
 from queue import PriorityQueue
 from copy import deepcopy
+from collections import deque
 
 
 class Grid:
@@ -160,6 +161,7 @@ class Graph:
     def __init__(self):
 
         self._edges = {}
+        self._values = {}
 
     def __getitem__(self, item):
         """Enables getting the weight of the edge between two vertices.
@@ -192,7 +194,7 @@ class Graph:
         """
         return list(self._edges[vert_i])
 
-    def add_edge(self, u, v, weight=1):
+    def add_edge(self, u, v, weight=1, value=None):
         """Adds an edge from vertex u to vertex v with an optional weight.
 
         Examples:
@@ -209,11 +211,36 @@ class Graph:
 
         if u not in self._edges:
             self._edges[u] = {}
+        if u not in self._values:
+            self._values[u] = value
 
         if isinstance(v, int):
             v = [v]
         for k in v:
             self._edges[u][k] = weight
+
+    def count_reachable_nodes(self, start_node, max_steps):
+        """Count the number of nodes reachable after the exact amount of max_steps."""
+        #visited = set()
+        visited = []
+        queue = set([(start_node, 0)])  # (Node, Distance)
+
+        while queue:
+            node, steps = queue.pop()
+            if steps == max_steps:
+                #visited.add(node)
+                visited.append(node)
+            else:
+                for neighbor in self.neighbors(node):
+                    queue.add((neighbor, steps + 1))
+
+        return len(visited)
+
+    def index(self, value):
+        """Return the node indices for a given value."""
+        for n, v in self._values.items():
+            if v == value:
+                return n
 
     def print(self):
         """Prints a representation of the graph. Lists each vertex and its corresponding edges with weights."""
@@ -221,30 +248,42 @@ class Graph:
             print(f'{e} -> {n}')
 
     @classmethod
-    def from_grid(cls, grid: 'Grid', nb_mask: 'Grid', decision_fn):
+    def from_grid(cls, grid: 'Grid', nb_mask: 'Grid', decision_fn, repeat_grid=None):
         """Convert a grid into a Graph, using the supplied neighborhood mask and decision function.
 
             The mask defines the considered local neighborhood of each grid cell with the respective pixel in the middle.
             The mask is a binary mask, with 1 stating the element should be considered, and 0 otherwise.
             The decision_fn is executed on each potential grid-pair and should return a positive value (an edge weight)
             if the two graph nodes are considered neighbors.
+            repeat_grid controls if the grid is allowed to repeat, i.e. nodes will connect to neighbors even if they
+            were beyond the grid
         """
-        new_graph = cls()
+        def __check_coords(coords):
+            return min(coords) >= 0 and coords[0] < grid.height and coords[1] >= 0 and coords[1] < grid.width
 
+        new_graph = cls()
+        # map coords to distinct indices
+        coord_i = []
         # relative neighbor coordinates
         nb_coords = [(x - nb_mask.shape[0] // 2, y - nb_mask.shape[1] // 2) for x in range(nb_mask.shape[0])
-                     for y in range(nb_mask.shape[1]) if nb_mask[(x, y)] == 1]
+                     for y in range(nb_mask.shape[1]) if nb_mask[x, y][0] == 1]
         nb_coords.remove((0, 0))
         for r in range(grid.height):
             for c in range(grid.width):
-                for nb_coord in nb_coords:
-                    # take care of grid borders
-                    nb = (r+nb_coord[0], c+nb_coord[1])
-                    if min(nb) < 0 or nb[0] >= grid.height or nb[1] >= grid.width:
-                        continue
+                neighbors = [(r+nb_coord[0], c+nb_coord[1]) for nb_coord in nb_coords]
+                if not repeat_grid:
+                    neighbors = [nb for nb in neighbors if __check_coords(nb)]
+                else:
+                    neighbors = [(nb[0]%grid.height, nb[1]%grid.width) for nb in neighbors]
+                for nb in neighbors:
                     weight = decision_fn(grid[(r, c)], grid[nb])
-                    if weight >= 0:
-                        new_graph.add_edge((r, c), nb, weight)
+                    if weight > 0:
+                        if (r, c) not in coord_i:
+                            coord_i.append((r, c))
+                        if nb not in coord_i:
+                            coord_i.append(nb)
+                        #new_graph.add_edge(coord_i.index((r, c)), coord_i.index(nb), weight, grid[(r, c)][0])
+                        new_graph.add_edge((r, c), [nb], weight, grid[(r, c)][0])
 
         return new_graph
 
@@ -395,6 +434,35 @@ def dijkstra(graph: 'Graph', start, end):
                     previous[nb_i] = cur_v
 
     return previous
+
+
+def dfs_longest_path(graph, start, end):
+    def dfs(node):
+        visited.add(node)
+        current_path.append(node)
+
+        if node == end:
+            nonlocal longest_path
+            if len(current_path) > len(longest_path):
+                longest_path = current_path.copy()
+        else:
+            for neighbor in graph.neighbors(node):
+                if neighbor not in visited:
+                    dfs(neighbor)
+
+        current_path.pop()
+        visited.remove(node)
+
+    visited = set()
+    current_path = []
+    longest_path = []
+
+    dfs(start)
+
+    if not longest_path:
+        return None  # No path exists
+    else:
+        return longest_path
 
 
 def parse_input(file='input.txt', convert_fn=None, split_on='\n'):
