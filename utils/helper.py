4d926f2354a2525cce3793ef51b8be73b128345b
Gordon Böer <Legor@users.noreply.github.com>
2024-12-06 11:45:55 +0100

    🏷️ add type hints

--- utils/helper.py
+++ utils/helper.py
@@ -3,9 +3,9 @@ import copy
 import math
 import re
 from functools import reduce
+from typing import Iterable, Union
 
-
-def get_mask(arr, cond):
+def get_mask(arr: list[list], cond):
     """For a 2-d list return a binary array of the same shape (a mask) according to the given condition"""
 
     mask = copy.deepcopy(arr)
@@ -15,19 +15,21 @@ def get_mask(arr, cond):
     return mask
 
 
-def l_to_i(li):
+def l_to_i(li: list):
     """Map all elements of a list to int"""
     return list(map(int, li))
 
 
-def chunks(xs, n):
+def chunks(xs: list, n: int):
     """split list into chunks of size n"""
     n = max(1, n)
-    return (xs[i : i + n] for i in range(0, len(xs), n))
+    return (xs[i: i + n] for i in range(0, len(xs), n))
 
 
-def bits_to_dec(arr):
+def bits_to_dec(arr: Iterable):
     """
+    Convert a binary pattern to the decimal representation
+
     [False, True, True] --> 3
     [1, 0, 1, 0] --> 6
     "0111" --> 7
@@ -36,7 +38,7 @@ def bits_to_dec(arr):
     return int("".join([str(int(x)) for x in arr]), 2)
 
 
-def prod(numbers):
+def prod(numbers: Iterable):
     """Return the product of all numbers in the given list."""
     y = 1
     for x in numbers:
@@ -44,21 +46,22 @@ def prod(numbers):
     return y
 
 
-def sign(x):
+def sign(x: Union[int, float]):
+    """Return the sign of the number (-1, or 1). Will return 1 for zero."""
     return -1 if x < 0 else 1
 
 
-def lcm(a, b):
+def lcm(a: int, b: int):
     """Calculate the least common multiple (LCM) for two numbers."""
     return abs(a * b) // math.gcd(a, b)
 
 
-def find_lcm(numbers: list):
+def find_lcm(numbers: list[int]):
     """Calculate the least common multiple (LCM) for a list of numbers."""
     return reduce(lcm, numbers)
 
 
-def get_nb_indices(y, x):
+def get_nb_indices(y: int, x: int):
     """Generate a list of 2d neighborhood indices (8-connected) for a given center coordinate.
     The center coordinate itself is excluded."""
     return [
